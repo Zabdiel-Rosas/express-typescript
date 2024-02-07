c0c747ed2c12cef2bc2404f7900333cf18bd63e2
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2024-02-07 16:50:08 -0600

    refactor(types): weather and visibility are now enums

---
+++ src/enums.ts
@@ -0,0 +1,14 @@
+export enum Weather {
+  Sunny = 'sunny',
+  Cloudy = 'cloudy',
+  Windy = 'windy',
+  Rainy = 'rainy',
+  Stormy = 'stormy'
+}
+
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor'
+}
